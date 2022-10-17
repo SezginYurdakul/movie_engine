e8694e87a2789d90859518911dd14e4afe84f4ec
Sezgin Yurdakul <sezginyurdakul2022@gmail.com>
2022-10-17 23:49:37 +0200

    11. commmit

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
+import { MovieContext } from "../contexts/MovieProvider";
+
 
 export default function MovieDetail() {
     const { id } = useParams();
     const [movie, setMovie] = useState([]);
-    const [error, setError] = useState('');
-    const [isLoading, setISLoading] = useState(true)
+
+    const { error, isLoading, setError, setIsLoading } = useContext(MovieContext);
+
     const API_key = "api_key=1b2e9155594c74034ae9f47ba073a1cd";
     const url = `https://api.themoviedb.org/3/movie/${id}?${API_key}`;
 
@@ -18,7 +21,7 @@ export default function MovieDetail() {
             } catch (error) {
                 setError(error.message);
             } finally {
-
+                setIsLoading(false);
             }
         };
         fetchDataMovieDetails();
@@ -29,40 +32,47 @@ export default function MovieDetail() {
     const BACKDROP_SIZE = "w1280";
 
     return (
-        <div className="detail-base">
-            <img
-                className="background-image"
-                src={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${movie?.backdrop_path}`}
-                alt={movie?.id}
-            />
-            <div className="movie-info-content">
-                <div className="detail-poster">
-                    <img
-                        className="poster-image"
-                        src={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${movie?.poster_path}`}
-                        alt={movie?.id}
-                    />
-                </div>
-                <div className="movie-info-text">
-                    <h1>
-                        {movie?.title} <span>({movie?.release_date?.substring(0, 4)})</span>
-                    </h1>
-                    <div className="genres">
-                        {movie?.genres?.map((genre) => (
-                            <span key={genre.name}>{genre.name} </span>
-                        ))}
-                    </div>
-                    <h3>Overview</h3>
-                    <p>{movie?.overview}</p>
 
-                    <div className="rating-director">
-                        <div>
-                            <h3>IMDB Rating </h3>
-                            <div className="score">{movie?.vote_average?.toFixed(1)}</div>
+
+        <>{isLoading ? (<h3 className="loading" > Movie Details are Loading ...</h3>)
+            : error ? (<h3 className="error">Something about API Link. Error : {error}</h3>)
+                : (<>
+                    <div className="detail-base">
+                        <img
+                            className="background-image"
+                            src={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${movie?.backdrop_path}`}
+                            alt={movie?.id}
+                        />
+                        <div className="movie-info-content">
+                            <div className="detail-poster">
+                                <img
+                                    className="poster-image"
+                                    src={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${movie?.poster_path}`}
+                                    alt={movie?.id}
+                                />
+                            </div>
+                            <div className="movie-info-text">
+                                <h1>
+                                    {movie?.title} <span>({movie?.release_date?.substring(0, 4)})</span>
+                                </h1>
+                                <div className="genres">
+                                    {movie?.genres?.map((genre) => (
+                                        <span key={genre.name}>{genre.name} </span>
+                                    ))}
+                                </div>
+                                <h3>Overview</h3>
+                                <p>{movie?.overview}</p>
+
+                                <div className="rating-director">
+                                    <div>
+                                        <h3>IMDB Rating </h3>
+                                        <div className="score">{movie?.vote_average?.toFixed(1)}</div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    );
+                </>)
+        }
+        </>);
 }
